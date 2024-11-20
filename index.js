bbaf412b101148b1d80631d262897595b2c33768
rajpal <pal51105@gmail.com>
2024-11-21 00:28:26 +0530

    initial commit before mail functionallity

---
+++ index.js
@@ -0,0 +1,46 @@
+//create app
+const e = require('express')
+const express = require('express')
+
+const app = express()
+
+require('dotenv').config()
+
+//Port find kro
+const PORT = process.env.PORT || 5000
+
+
+//middleware add kro
+app.use(express.json())
+
+const fileupload = require("express-fileupload")
+
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}))
+
+
+//Connect with Db
+require('./config/database').dbConnection()
+
+//connect with cloudinary
+require('./config/cloudnary').cloudinaryConnetion()
+
+
+//mount api routes
+
+const upload = require('./routers/fileUpload')
+
+app.use('/api/v1/upload',upload)
+
+// activate server
+
+app.listen(PORT,()=>{
+    console.log(`App is listen at ${PORT}`)
+})
+
+app.get('/',(req,res)=>{
+    res.send('I love you Kake')
+    console.log('This is default route')
+})
