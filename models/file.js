8151e0792992e151786ab45daca5a5ee842b137c
rajpal <pal51105@gmail.com>
2024-11-21 23:48:06 +0530

    committing modified files

--- models/file.js
+++ models/file.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require('nodemailer')
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -18,7 +19,37 @@ const fileSchema = new mongoose.Schema({
 
 //post middleware
 
+fileSchema.post("save",async function(doc) {
+    try {
+        
 
+        const transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+
+        });
+        // sending mail 
+
+        let info = await transporter.sendMail({
+            from: '"LearnJet" <no-reply@learnjet.com>', // Sender's details
+            to: doc.email,                              // Recipient's email
+            subject: "New File Uploaded to Cloudinary", // Email subject
+            html: `
+                <h2>Hello!</h2>
+                <p>A new file has been uploaded. You can view it by clicking the link below:</p>
+                <a href="${doc.imageUrl}" target="_blank">${doc.imageUrl}</a>
+            `, // HTML content of the email
+        });
+        
+        
+        
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 const File = mongoose.model('File',fileSchema)
 module.exports = File
